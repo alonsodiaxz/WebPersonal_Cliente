6efa27488fcb8c2df366023f2d4ef24ec7e79f02
alonsodiaxz <alonsodiazsobrino@hotmail.es>
2021-08-04 23:41:13 +0200

    Almacenar datos en el localStorage

--- src/components/Admin/LoginForm/LoginForm.js
+++ src/components/Admin/LoginForm/LoginForm.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
 import "./LoginForm.scss";
-import { Form, Input, Button, notification } from "antd";
+import { Form, Input, Button, notification, Checkbox } from "antd";
 import { UserOutlined, LockOutlined } from "@ant-design/icons";
 import { SignInAPI } from "../../../API/user";
+import { ACCESS_TOKEN, REFRESH_TOKEN } from "../../../utils/constants";
 
 export default function LoginForm() {
   const { Item } = Form;
@@ -10,28 +11,54 @@ export default function LoginForm() {
   const [Login, setLogin] = useState({
     email: null,
     password: null,
+    LoginRemember: false,
   });
 
   const login = async (e) => {
     e.preventDefault();
-    const peticion = await SignInAPI(Login);
-
-    if (peticion.accessToken) {
-      notification["success"]({
-        message: `${peticion.message}`,
+    if (!Login.email || !Login.password) {
+      notification["error"]({
+        message: "Todos los campos son obligatorios.",
       });
     } else {
-      notification["error"]({
-        message: `${peticion.message}`,
-      });
+      const peticion = await SignInAPI(Login);
+
+      if (peticion.accessToken) {
+        notification["success"]({
+          message: `${peticion.message}`,
+        });
+        const { accessToken, refreshToken } = peticion;
+        const localStorage1 = "email";
+        const localStorage2 = "password";
+        if (Login.LoginRemember) {
+          localStorage.setItem(ACCESS_TOKEN, accessToken);
+          localStorage.setItem(REFRESH_TOKEN, refreshToken);
+          localStorage.setItem(localStorage1, Login.email);
+          localStorage.setItem(localStorage2, Login.password);
+        }
+
+        //Una vez el usuario se logea le hace la redirección hacia el /admin
+        /*window.location.href = "/admin";*/
+      } else {
+        notification["error"]({
+          message: `${peticion.message}`,
+        });
+      }
     }
   };
 
   const changeForm = (ev) => {
-    setLogin({
-      ...Login,
-      [ev.target.name]: ev.target.value,
-    });
+    if (ev.target.name === "LoginRemember") {
+      setLogin({
+        ...Login,
+        [ev.target.name]: ev.target.checked,
+      });
+    } else {
+      setLogin({
+        ...Login,
+        [ev.target.name]: ev.target.value,
+      });
+    }
   };
 
   return (
@@ -43,6 +70,7 @@ export default function LoginForm() {
           name="email"
           placeholder="Correo electrónico"
           className="login-form__input"
+          value={Login.email}
         />
       </Item>
       <Item>
@@ -52,7 +80,13 @@ export default function LoginForm() {
           name="password"
           placeholder="Contraseña"
           className="login-form__input"
+          value={Login.password}
         ></Input>
+      </Item>
+      <Item>
+        <Checkbox name="LoginRemember" type="checkbox">
+          ¿Desea guardar los datos del último inicio de sesión?
+        </Checkbox>
       </Item>
       <Item>
         <Button htmlType="submit" className="login-form__button">
