295b03ee63c6e35902086b20cdf49008800642f6
alonsodiaxz <alonsodiazsobrino@hotmail.es>
2021-08-14 18:18:21 +0200

    Creación de formulario para editar usuarios

--- src/components/Admin/Users/EditUserForm/EditUserForm.js
+++ src/components/Admin/Users/EditUserForm/EditUserForm.js
@@ -6,6 +6,8 @@ import { useDropzone } from "react-dropzone";
 import { useState } from "react";
 import { useCallback } from "react";
 import NoAvatar from "../../../../assets/img/png/no-avatar.png";
+import { UserOutlined, LockOutlined, MailOutlined } from "@ant-design/icons";
+import { useEffect } from "react";
 
 export default function EditUserForm(props) {
   const { user } = props;
@@ -14,9 +16,19 @@ export default function EditUserForm(props) {
     name: user.name,
     lastname: user.lastname,
     email: user.email,
+    password: user.password,
     role: user.role,
     avatar: user.avatar,
   });
+
+  useEffect(() => {
+    if (avatar) {
+      setUserData({
+        ...userData,
+        avatar: avatar,
+      });
+    }
+  }, [avatar]);
 
   const updateUser = (ev) => {
     ev.preventDefault();
@@ -28,9 +40,9 @@ export default function EditUserForm(props) {
       <UploadAvatar avatar={avatar} setAvatar={setAvatar} />
       <h2> {user.email}</h2>
       <EditForm
-        user={user}
         userData={userData}
         setUserData={setUserData}
+        updateUser={updateUser}
       ></EditForm>
     </div>
   );
@@ -66,20 +78,107 @@ function UploadAvatar(props) {
 }
 
 function EditForm(props) {
-  const { user, userData, setUserData } = props;
+  const { userData, setUserData, updateUser } = props;
   const { Item } = Form;
-  console.log(user);
+  const { Option } = Select;
 
   return (
-    <Form className="edit-user-form">
+    <Form className="edit-user-form" onSubmitCapture={updateUser}>
+      <Row gutter={24}>
+        <Col span={12}>
+          <Item>
+            <Input
+              prefix={<UserOutlined style={{ color: "rgba(0,0,0,.25)" }} />}
+              placeholder="Nombre"
+              className="edit-user-form__input"
+              defaultValue={userData.name}
+              onChange={(e) =>
+                setUserData({ ...userData, name: e.target.value })
+              }
+            />
+          </Item>
+        </Col>
+        <Col span={12}>
+          <Item>
+            <Input
+              prefix={<UserOutlined style={{ color: "rgba(0,0,0,.25)" }} />}
+              placeholder="Apellidos"
+              className="edit-user-form__input"
+              defaultValue={userData.lastname}
+              onChange={(e) =>
+                setUserData({ ...userData, lastname: e.target.value })
+              }
+            />
+          </Item>
+        </Col>
+      </Row>
+      <Row gutter={24}>
+        <Col span={12}>
+          <Item>
+            <Input
+              prefix={<MailOutlined style={{ color: "rgba(0,0,0,.25)" }} />}
+              placeholder="Correo electrónico"
+              className="edit-user-form__input"
+              defaultValue={userData.email}
+              onChange={(e) =>
+                setUserData({ ...userData, email: e.target.value })
+              }
+            />
+          </Item>
+        </Col>
+        <Col span={12}>
+          <Select
+            placeholder="Selecciona un rol"
+            onChange={(ev) =>
+              setUserData({
+                ...userData,
+                role: ev,
+              })
+            }
+            defaultValue={userData.role}
+          >
+            <Option value="admin"> Administrador</Option>
+            <Option value="editor"> Editor</Option>
+            <Option value="reviewer"> Revisor</Option>
+          </Select>
+        </Col>
+      </Row>
+      <Row gutter={24}>
+        <Col span={12}>
+          <Item>
+            <Input
+              prefix={<LockOutlined style={{ color: "rgba(0,0,0,.25)" }} />}
+              type="password"
+              placeholder="Contraseña"
+              onChange={(e) =>
+                setUserData({
+                  ...userData,
+                  password: e.target.value,
+                })
+              }
+            ></Input>
+          </Item>
+        </Col>
+        <Col span={12}>
+          <Item>
+            <Input
+              prefix={<LockOutlined style={{ color: "rgba(0,0,0,.25)" }} />}
+              type="password"
+              placeholder="Repetir contraseña"
+              onChange={(e) =>
+                setUserData({
+                  ...userData,
+                  repeatPassword: e.target.value,
+                })
+              }
+            ></Input>
+          </Item>
+        </Col>
+      </Row>
       <Item>
-        <Input
-          type="name"
-          name="name"
-          placeholder="Nombre"
-          className="edit-user-form__input"
-          value={user.name}
-        />
+        <Button type="primary" htmlType="submit">
+          Actualizar usuario
+        </Button>
       </Item>
     </Form>
   );
